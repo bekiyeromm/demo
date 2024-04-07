959b6e02e6338431ecb9e770d8c36bd9a902f68a
berekettena <tena21bereket@gmail.com>
2024-04-07 10:34:08 +0300

    print out statement added

--- calc.py
+++ calc.py
@@ -1,10 +1,10 @@
 def add(a, b):
 	return (a +b)
-add(4,5)
+print("sum",add(4,5))
 
 def sub(a,b):
-    retrun (a-b)
-sub(4,5)
+    return(a-b)
+print("difference",sub(4,5))
 def mult(a,b):
-	return (a *b )
+	return (a *b)
 mult(2,5)
